b3ac5880c5356b7dba1f682deb8e83c04c36f15a
Avaneesh Kumar <avnikumar94@gmail.com>
2019-04-18 16:26:19 +0530

    index.js && info.php

---
+++ index.js
@@ -0,0 +1,21 @@
+<?php
+$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+foreach($cars as $keys=>$values)
+{
+foreach ($values as $keys=>$data)
+{
+echo $key. " " .$keys. " ".$keys. " ";
+}
+
+
+}
+
+
+
+?>
